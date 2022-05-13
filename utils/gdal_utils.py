a7673597f0712921ddddb327567a476fc3d57859
zmoka-zht <zmoka.zht@gmail.com>
2022-05-13 21:18:26 +0800

    get_geo_transfor

--- utils/gdal_utils.py
+++ utils/gdal_utils.py
@@ -16,7 +16,22 @@ def get_img_shape(data_file:str)->tuple:
 
     return img_shape
 
+def get_geo_transformer(data_file:str):
+    '''
+    通过gdal获取遥感图像经纬度
+    :param data_file:遥感图像路径
+    :return:
+    '''
+    dataset = gdal.Open(data_file)
+    if dataset is None:
+        print("文件%s无法打开" % data_file)
+        exit(-1)
+    geo_transformer = dataset.GetGeoTransform()
+    return geo_transformer
+
 if __name__=="__main__":
     data_file = r'/data02/zht_vqa/change_detection/LearnGroup/S2A_MSIL2A_20200810T030551_N9999_R075_T50SLJ_m10.tiff'
-    img_shape = get_img_shape(data_file)
-    print(img_shape)
+    # img_shape = get_img_shape(data_file)
+    # print(img_shape)
+    geo_trans = get_geo_transformer(data_file)
+    print(geo_trans)
