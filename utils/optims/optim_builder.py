51b4922a0bdca55046eed6ac96498b96d4048cc9
zmoka-zht <zmoka.zht@gmail.com>
2022-05-14 10:07:52 +0800

    update_optim

--- utils/optims/optim_builder.py
+++ utils/optims/optim_builder.py
@@ -6,17 +6,18 @@ optims_dict = {'SGD':optim.SGD,
                'Adam':optim.Adam}
 
 #创建bulider函数实例化optim
-# def build_optim(name:str='SGD', **kwargs):
-#     if name in optims_dict.keys():
-#         return optims_dict[name](**kwargs)
-#     else:
-#         raise NotImplementedError("name %s not available values.".format(name))
+def build_optim(name:str='SGD', **kwargs):
+    '''
 
-def build_optim(name='Adam',**kwargs):
+    :param name:
+    :param kwargs:
+    :return:
+    '''
     if name in optims_dict.keys():
         return optims_dict[name](**kwargs)
     else:
-        raise NotImplementedError('name not in availables values.'.format(name))
+        raise NotImplementedError("name %s not available values.".format(name))
+
 if __name__=='__main__':
 
     opt = build_optim('SGD')
