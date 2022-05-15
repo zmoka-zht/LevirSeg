a7aad665e91c380623b37632865137c0ea5c660b
zmoka-zht <zmoka.zht@gmail.com>
2022-05-15 17:54:39 +0800

    seg

--- config/seg.py
+++ config/seg.py
@@ -9,9 +9,13 @@ config = dict(
     train_cfig = dict(
         batch_size = 16,
         checkpoint_path =r'checkpoint/seg',
+        lr = 0.001,
+        device = 'cuda:0',
+        log_path = r'checkpoint/seg/log',
         train_data = dict(
-            root_path = r'',
-            data_files = r'',
+            root_path = r'/data02/zht_vqa/change_detection/LearnGroup/dataset/Vaihingen',
+            train_data_files = r'/data02/zht_vqa/change_detection/LearnGroup/dataset/Vaihingen/train.txt',
+            val_data_files =r'/data02/zht_vqa/change_detection/LearnGroup/dataset/Vaihingen/valid.txt'
         ),
 
     interface_cifg = dict()
