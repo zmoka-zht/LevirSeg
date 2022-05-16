73f2dd99f499d16e11ac41ec5496c53392a08015
zmoka-zht <zmoka.zht@gmail.com>
2022-05-16 11:19:28 +0800

    trainer

--- trainer.py
+++ trainer.py
@@ -1,6 +1,7 @@
 import argparse
 import mmcv
 from model.builder import build_model
+from interface.seg_interface import SegInterface
 
 parse = argparse.ArgumentParser()
 parse.add_argument('--config_file',default=r'config/seg.py',type=str)
@@ -8,8 +9,10 @@ parse.add_argument('--config_file',default=r'config/seg.py',type=str)
 
 if __name__=='__main__':
     args =parse.parse_args()
-    cfg = mmcv.Config.fromfile(args.config_file)
-    #print(**cfg['config']['model_config'])
-    model_cfg = cfg['config']['model_config']
-    model = build_model(model_cfg['name'])
-    print(model)
+    seg_train = SegInterface(args)
+    seg_train.tain()
+    # cfg = mmcv.Config.fromfile(args.config_file)
+    # print(**cfg['config']['model_config'])
+    # model_cfg = cfg['config']['model_config']
+    # model = build_model(model_cfg['name'])
+    # print(model)
