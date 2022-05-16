806380fb3f26688f0ae31c888db5449318e5841d
zmoka-zht <zmoka.zht@gmail.com>
2022-05-16 11:19:08 +0800

    update_seg_config

--- config/seg.py
+++ config/seg.py
@@ -1,17 +1,21 @@
 config = dict(
 
     model_config = dict(
-        name = 'seg_net',
+        name = 'seg_netz',
         in_channel = 3,
         out_channel = 6,
     ),
 
     train_cfig = dict(
         batch_size = 16,
-        checkpoint_path =r'checkpoint/seg',
+        num_epoch = 100,
+        checkpoint_path =r'/home/Wuwei/WuweiStuH/PycharmProjects/LevirSeg/checkpoint/seg',
         lr = 0.001,
         device = 'cuda:0',
-        log_path = r'checkpoint/seg/log',
+        log_path = r'/home/Wuwei/WuweiStuH/PycharmProjects/LevirSeg/checkpoint/seg',
+        criterion = 'CrossEntropyLoss',
+        optimer = 'SGD',
+        num_workers = 0,
         train_data = dict(
             root_path = r'/data02/zht_vqa/change_detection/LearnGroup/dataset/Vaihingen',
             train_data_files = r'/data02/zht_vqa/change_detection/LearnGroup/dataset/Vaihingen/train.txt',
